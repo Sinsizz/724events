096cd19714e4e1bb996b840bb63879c2721fb5f6
Théo Rouillard <theo.rouillard@outlook.fr>
2024-08-06 15:57:50 +0200

    Modification du containers Form

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -16,6 +16,7 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
+        onSuccess(); // Appeler onSuccess après une soumission réussie
       } catch (err) {
         setSending(false);
         onError(err);
